e110e5ecfb3c3b9d0068bb28c384e08283e444ad
Radim Sedláček <radim.sed@email.cz>
2025-04-15 15:13:13 +0200

    Finished version 1

---
+++ src/main/java/com/jam/JamApplication.java
@@ -0,0 +1,12 @@
+package com.jam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JamApplication.class, args);
+	}
+}
